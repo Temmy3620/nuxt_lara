cd10c8519ec80d895be48585c0bfd0c63770cf2a
Temmy3620 <me512papiko512@gmail.com>
2024-02-29 00:10:20 +0900

    掲示板の画面作成

--- nuxt_pj/layouts/default.vue
+++ nuxt_pj/layouts/default.vue
@@ -24,8 +24,10 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
+    
     <v-app-bar :clipped-left="clipped" fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <!--
       <v-btn icon @click.stop="miniVariant = !miniVariant">
         <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
       </v-btn>
@@ -40,7 +42,9 @@
       <v-btn icon @click.stop="rightDrawer = !rightDrawer">
         <v-icon>mdi-menu</v-icon>
       </v-btn>
+      -->
     </v-app-bar>
+    
     <v-main>
       <v-container>
         <Nuxt />
@@ -56,42 +60,49 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
+    <!--
     <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
+    -->
   </v-app>
 </template>
 
 <script>
-export default {
-  name: 'DefaultLayout',
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/',
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'todo',
-          to: '/todo',
-        },
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
-    }
-  },
-}
+  export default {
+    name: 'DefaultLayout',
+    data() {
+      return {
+        clipped: false,
+        drawer: false,
+        fixed: false,
+        items: [
+          {
+            icon: 'mdi-apps',
+            title: 'Welcome',
+            to: '/',
+          },
+          {
+            icon: 'mdi-chart-bubble',
+            title: 'Inspire',
+            to: '/inspire',
+          },
+          {
+            icon: 'mdi-chart-bubble',
+            title: 'todo',
+            to: '/todo',
+          },
+          {
+            icon: 'mdi-forum',
+            title: 'bulletinboard',
+            to: '/bulletinboard',
+          },
+        ],
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
+        title: 'Vuetify.js',
+      }
+    },
+  }
 </script>
---
+++ nuxt_pj/pages/bulletinboard.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-row justify="center" align="center">
+      <v-col cols="12" sm="12" md="12">
+        
+        <v-virtual-scroll
+          :item-height="100"
+          :height="600"
+          :items="['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30']"
+        >
+          <template v-slot:default="{ item }">
+            Item {{ item }}
+          </template>
+        </v-virtual-scroll>
+
+        <v-col cols="12" sm="11" md="11" class="mt-10 mb-10"></v-col>
+
+        <v-card class="fixed-card">
+          <v-card-title>Fixed Card</v-card-title>
+          <v-card-text>
+            This card will stay fixed at a specific position on the window, even when scrolling.
+          </v-card-text>
+        </v-card>
+      </v-col>
+      
+      
+    </v-row>
+</template>
+
+<script>
+  export default {
+    name: 'BulletinBoardPage',
+    
+  }
+</script>
+
+<style scoped>
+.fixed-card {
+  position: absolute;
+  bottom: calc(0px); /* ウィンドウの縦方向の中央から100px上に配置 */
+  left: calc(0px); /* ウィンドウの横方向の中央から150px左に配置 */
+  width: 100vw; /* 幅を指定 */
+  height: 150px;
+}
+</style>
+
+
+  
